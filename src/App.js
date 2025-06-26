6cdc035c47816037f092e18cd03cd50438e3692d
Seigo Ohata <seigo.ohata@microworld.co.jp>
2025-06-26 12:24:30 +0900

    App.js を編集してデプロイ用に更新

--- src/App.js
+++ src/App.js
@@ -1,25 +1,49 @@
-import logo from './logo.svg';
+import {useState} from 'react';
 import './App.css';
+import logo from './logo.svg';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+
+function App(){
+  const[task, setTask ] = useState('');
+  const[tasks, setTasks] = useState([]);
+
+  const addTask = () => {
+    if(task.trim()==='')return;
+    setTasks([...tasks,task]);
+    setTask('');
+  };
+
+  const deleteTask = (index) => {
+    const newTasks = tasks.filter((_, i) => i !== index);
+    setTasks(newTasks);};
+
+  return(
+    <div className='App'>
+      <h1>今日のやること</h1>
+      <input
+      type="text"
+      value={task}
+      onChange={(e)=>setTask(e.target.value)}
+      placeholder="新しいタスクを入力"
+      />
+      <button onClick={addTask}>追加</button>
+      
+
+
+      <ul>
+  {tasks.map((task, index) => (
+    <li key={index} className="task-item">
+      ✅ {task}
+      <button onClick={() => deleteTask(index)}>削除</button>
+    </li>
+  ))}
+</ul>
+
+     <img src={logo} className="App-logo" alt="ロゴ" />
     </div>
   );
+
+
 }
 
 export default App;
